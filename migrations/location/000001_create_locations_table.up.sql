2f0388afdd0abedc4e4b2b8227abce8d5348dde2
Andrew Shubnikov <a.shbov@yandex.ru>
2023-12-02 16:18:17 +0300

    [location-service] fix locations migration

--- migrations/location/000001_create_locations_table.up.sql
+++ migrations/location/000001_create_locations_table.up.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS locations
 (
-    id   UUID PRIMARY KEY,
-    name VARCHAR(255),
-    auto VARCHAR(255),
-    lat  NUMERIC,
-    lng  NUMERIC
+    id         SERIAL PRIMARY KEY,
+    driver_id  UUID    NOT NULL,
+
+    lat        NUMERIC NOT NULL,
+    lng        NUMERIC NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX driver_index ON locations (driver_id);
