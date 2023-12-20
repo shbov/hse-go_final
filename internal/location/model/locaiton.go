6932e954184fbac2f48a222162f5344c3b1bd486
Maxim Ostashkov <max23.ost@gmail.com>
2023-12-20 19:23:13 +0300

    alter schema of location

--- internal/location/model/locaiton.go
+++ internal/location/model/locaiton.go
@@ -1,13 +1,9 @@
 package model
 
-import "time"
-
 type Location struct {
 	Id       int    `json:"id"`
 	DriverId string `json:"driver_id"`
 
-	Lat float32 `json:"lat"` // float - ok?
+	Lat float32 `json:"lat"`
 	Lng float32 `json:"lng"`
-
-	CreatedAt time.Time `json:"created_at"` // time.Time - ok? mb timestamp (other libs)?
 }
